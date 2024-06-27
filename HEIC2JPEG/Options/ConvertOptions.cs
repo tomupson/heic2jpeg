2dfb63b5882d9a092d16f806bc85e167e9e93524
Tom Upson <30987275+tomupson@users.noreply.github.com>
2024-06-27 11:13:34 +0100

    Add project

---
+++ HEIC2JPEG/Options/ConvertOptions.cs
@@ -0,0 +1,13 @@
+﻿using CommandLine;
+
+namespace HEIC2JPEG.Options;
+
+[Verb("convert", HelpText = "Convert a .heic to a .jpg")]
+internal sealed class ConvertOptions
+{
+    [Option('d', "delete", Default = true, HelpText = "Determines whether or not to delete the original file")]
+    public required bool Delete { get; set; }
+
+    [Value(0, HelpText = "The file or directory to convert. Defaults to the current directory if unspecified")]
+    public required string FileOrDirectory { get; set; }
+}
---
+++ HEIC2JPEG/Program.cs
@@ -0,0 +1,67 @@
+﻿using CommandLine;
+using HEIC2JPEG.Options;
+using ImageMagick;
+
+namespace HEIC2JPEG;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        Parser.Default.ParseArguments<ConvertOptions>(args)
+            .WithParsed(ConvertFiles);
+    }
+
+    private static void ConvertFiles(ConvertOptions options)
+    {
+        if (File.Exists(options.FileOrDirectory))
+        {
+            ProcessFile(options.FileOrDirectory, options);
+            return;
+        }
+
+        if (string.IsNullOrWhiteSpace(options.FileOrDirectory))
+        {
+            options.FileOrDirectory = Directory.GetCurrentDirectory();
+        }
+
+        if (!Directory.Exists(options.FileOrDirectory))
+        {
+            return;
+        }
+
+        foreach (string file in Directory.EnumerateFiles(options.FileOrDirectory, "*.heic", SearchOption.AllDirectories))
+        {
+            ProcessFile(file, options);
+        }
+    }
+
+    private static void ProcessFile(string file, ConvertOptions options)
+    {
+        if (!Path.GetExtension(file).Equals(".heic", StringComparison.OrdinalIgnoreCase))
+        {
+            return;
+        }
+
+        Console.WriteLine($"Processing file: {file}");
+
+        using MagickImage image = new MagickImage(file);
+
+        try
+        {
+            string newFileName = file.Replace(Path.GetExtension(file), ".jpg");
+            image.Write(newFileName);
+
+            if (options.Delete)
+            {
+                File.Delete(file);
+            }
+        }
+        catch
+        {
+            Console.WriteLine($"Failed to process file: {file}");
+        }
+
+        Console.WriteLine($"Successfully processed{(options.Delete ? " and deleted" : string.Empty)} file: {file}");
+    }
+}
